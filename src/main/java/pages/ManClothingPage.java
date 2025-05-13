1993e984673a2b887cfe5239386a94ef8cb786e0
NK0810 <nkulikov0810@gmail.com>
2025-05-13 16:05:57 +0300

    Add filterBySalesTest

--- src/main/java/pages/ManClothingPage.java
+++ src/main/java/pages/ManClothingPage.java
@@ -12,6 +12,7 @@ public class ManClothingPage extends ProductCatalogPage {
     private static final String URL = BASE_URL + "/cholovik/cholovichij-odjag";
     private static final String PRODUCT_PRICES = "//div[@class='c-price__current']";
     private static final String NEW_TAG_ELEMENTS = "//span[@class='product-card__badge product-card__badge--new']";
+    private static final String PRODUCT_PRICES_WITHOUT_DISCOUNT = "//span[contains(text(), 'Звичайна ціна')]";
 
     public ManClothingPage(WebDriver driver) {
         super(driver);
@@ -51,5 +52,15 @@ public class ManClothingPage extends ProductCatalogPage {
     @Step("Waits util new tag are updated")
     public List<WebElement> waitUntilTagsAreUpdated(){
         return waitElementsAreUpdated(By.xpath(NEW_TAG_ELEMENTS));
+    }
+
+    @Step("Get visible product prices as text")
+    public List<String> getVisiblePriceWithoutDiscountTexts() {
+        By priceLocator = By.xpath(PRODUCT_PRICES_WITHOUT_DISCOUNT);
+
+        return waitElementsAreVisible(priceLocator)
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/tests/ProductFilterTests.java
+++ src/test/java/tests/ProductFilterTests.java
@@ -9,6 +9,7 @@ import pages.ManClothingPage;
 import java.util.List;
 
 import static pages.ProductCatalogPage.FilterOption.NEW_ARRIVALS;
+import static pages.ProductCatalogPage.FilterOption.SALE;
 
 public class ProductFilterTests extends BaseTest {
     @Description("Verify that filtering by 'New arrivals' only displays products marked.")
@@ -28,5 +29,36 @@ public class ProductFilterTests extends BaseTest {
         Assert.assertTrue(
                 productNewTagsFilterByNewArrivals.stream().allMatch(tag -> tag.contains("Новий")), "Not all product tags contain 'Новий'"
         );
+    }
+
+    @Description("Verify that the discount filter correctly displays only products with active discounts.")
+    @Test
+    public void filterBySalesTest() {
+        ManClothingPage manClothingPage = new ManClothingPage(driver);
+
+        manClothingPage
+                .openUrl()
+                .acceptCookies()
+                .filterByOption(SALE);
+        manClothingPage
+                .waitUntilProductPricesAreUpdated();
+
+        List<String> priceProductsWithDiscount = manClothingPage.getVisiblePriceTexts();
+        List<String> priceProductsWithoutDiscount = manClothingPage.getVisiblePriceWithoutDiscountTexts();
+
+        Assert.assertTrue(isDiscountApplied(priceProductsWithoutDiscount, priceProductsWithDiscount) , "Discount not applied: discounted price is not lower than the regular price");
+    }
+
+    private boolean isDiscountApplied(List<String> priceWithoutDiscount, List<String> priceWithDiscount) {
+        for (int i = 0; i < priceWithDiscount.size(); i++) {
+            if (toInt(priceWithDiscount.get(i)) < toInt(priceWithoutDiscount.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int toInt(String text) {
+        return Integer.parseInt(text.replaceAll(",.*", "").replaceAll("[^\\d]", ""));
     }
 }
