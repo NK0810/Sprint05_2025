45e80b7faf396ad60e51faf3da4814d39c8016e9
NK0810 <nkulikov0810@gmail.com>
2025-05-14 11:24:16 +0300

    Made changes SCRUM86-2

--- src/main/java/pages/ManClothingPage.java
+++ src/main/java/pages/ManClothingPage.java
@@ -12,7 +12,7 @@ public class ManClothingPage extends ProductCatalogPage {
     private static final String URL = BASE_URL + "/cholovik/cholovichij-odjag";
     private static final String PRODUCT_ACTUAL_PRICES = "//div[@class='c-price__current']";
     private static final String NEW_TAG_ELEMENTS = "//span[@class='product-card__badge product-card__badge--new']";
-    private static final String PRODUCT_PRICES_WITHOUT_DISCOUNT = "//span[contains(text(), 'Звичайна ціна')]";
+    private static final String PRODUCT_PRICES_REGULAR_DISCOUNT = "//span[contains(text(), 'Звичайна ціна')]";
 
     public ManClothingPage(WebDriver driver) {
         super(driver);
@@ -52,7 +52,7 @@ public class ManClothingPage extends ProductCatalogPage {
 
     @Step("Get visible product prices as text")
     public List<String> getVisibleRegularPriceTexts() {
-        return waitElementsAreVisible(By.xpath(PRODUCT_PRICES_WITHOUT_DISCOUNT))
+        return waitElementsAreVisible(By.xpath(PRODUCT_PRICES_REGULAR_DISCOUNT))
                 .stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
