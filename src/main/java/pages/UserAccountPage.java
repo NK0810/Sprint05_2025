0a0b73cc280f60e882ebe8604184fbb0b4b26ff4
[PROJECT <[PROJECT>
2025-05-16 16:31:01 +0300

    write EditPersonalData.java test

--- src/main/java/pages/UserAccountPage.java
+++ src/main/java/pages/UserAccountPage.java
@@ -10,6 +10,15 @@ public class UserAccountPage extends BasePage<UserAccountPage> {
     private static final By MY_ACCOUNT_HEADER = xpath("//*[@data-ui-id='page-title-wrapper']");
     private static final By MY_ACCOUNT_EMAIL = xpath("//p[@class='dashboard-info-block__email']");
     private static final By TEXT_OUT = xpath("//*[@class='nav items']/li[10]/a");
+    private static final String ACCOUNT_SETTING = "//*[@class='nav item'][4]/a";
+    private static final String FIRST_NAME_FIELD = "//input[@name='firstname']";
+    private static final String SUR_NAME_FIELD = "//input[@name='lastname']";
+    private static final String NOTIFICATION_CHECK_BOX_MAIL_LABEL = "//label[@for='gdpr-agreement-f56754fe-fa50-409a-9ec2-8706631db48c']";
+    private static final String NOTIFICATION_CHECK_BOX_PHONE_LABEL = "//label[@for='gdpr-agreement-acab0796-e6e9-41d1-bbd3-9af8c46e277f']";
+    private static final String NOTIFICATION_CHECK_BOX_MAIL_INPUT = "//input[@id='gdpr-agreement-f56754fe-fa50-409a-9ec2-8706631db48c']";
+    private static final String NOTIFICATION_CHECK_BOX_PHONE_INPUT = "//input[@id='gdpr-agreement-acab0796-e6e9-41d1-bbd3-9af8c46e277f']";
+    private static final String SAVE_CHANGE_BUTTON = "//button[@class='action save account-edit-info__button button button--regular button__primary']";
+    private static final By SUCCESS_MESSAGE = xpath("//div[@class='message-text' and contains(text(), 'Дані Вашого Облікового запису збережено')]");
 
     public UserAccountPage(WebDriver driver) {
         super(driver);
@@ -31,4 +40,89 @@ public class UserAccountPage extends BasePage<UserAccountPage> {
         String text = waitElementIsVisible(TEXT_OUT).getText();
         return text;
     }
+
+    @Step("Click My Account Setting")
+    public UserAccountPage clickAccountSetting() {
+        waitElementToBeClickable(By.xpath(ACCOUNT_SETTING)).click();
+        return this;
+    }
+
+    @Step("Click and clear first name field")
+    public UserAccountPage clickAndClearFirstNameField() {
+        waitElementIsVisible(By.xpath(FIRST_NAME_FIELD)).clear();
+        waitElementIsVisible(By.xpath(FIRST_NAME_FIELD)).click();
+        return this;
+    }
+
+    @Step("Input New Test Name")
+    public UserAccountPage sendNewTestName(String name) {
+        waitElementIsVisible(By.xpath(FIRST_NAME_FIELD)).sendKeys(name);
+        return this;
+    }
+
+    @Step("Click and clear surname field")
+    public UserAccountPage clickAndClearSurnameField() {
+        waitElementIsVisible(By.xpath(SUR_NAME_FIELD)).clear();
+        waitElementIsVisible(By.xpath(SUR_NAME_FIELD)).click();
+        return this;
+    }
+
+    @Step("Input New Test Surname")
+    public UserAccountPage sendNewTestSurname(String name) {
+        waitElementIsVisible(By.xpath(SUR_NAME_FIELD)).sendKeys(name);
+        return this;
+    }
+
+    @Step("Scroll to save change button")
+    public UserAccountPage scrollToSaveChangeButton() {
+        scrollToElement(By.xpath(SAVE_CHANGE_BUTTON));
+        return null;
+    }
+
+    @Step("Click Check Box Mail Notification")
+    public UserAccountPage clickCheckBoxMail() {
+        waitElementIsVisible(By.xpath(NOTIFICATION_CHECK_BOX_MAIL_LABEL)).click();
+        return this;
+    }
+
+    @Step("Click Check Box Phone Notification")
+    public UserAccountPage clickCheckBoxPhone() {
+        waitElementIsVisible(By.xpath(NOTIFICATION_CHECK_BOX_PHONE_LABEL)).click();
+        return this;
+    }
+
+    @Step("Click save change")
+    public UserAccountPage clickSaveChange() {
+        waitElementToBeClickable(By.xpath(SAVE_CHANGE_BUTTON)).click();
+        return this;
+    }
+
+    @Step("Get current first name")
+    public String getFirstNameFieldText() {
+        return waitElementIsVisible(By.xpath(FIRST_NAME_FIELD)).getAttribute("value");
+    }
+
+    @Step("Get current surname")
+    public String getSurnameFieldText() {
+        return waitElementIsVisible(By.xpath(SUR_NAME_FIELD)).getAttribute("value");
+    }
+
+
+    @Step("Get success message text after saving changes")
+    public String getSuccessMessageText() {
+        return waitElementIsVisible(SUCCESS_MESSAGE).getText();
+
+    }
+
+    @Step("Check if checkbox 1 is selected")
+    public boolean isCheckBoxMailSelected() {
+        return waitPresenceOfElement(By.xpath(NOTIFICATION_CHECK_BOX_MAIL_INPUT)).isSelected();
+    }
+
+    @Step("Check if checkbox 2 is selected")
+    public boolean isCheckBoxPhoneSelected() throws InterruptedException {
+        Thread.sleep(1000);
+        return waitPresenceOfElement(By.xpath(NOTIFICATION_CHECK_BOX_PHONE_INPUT)).isSelected();
+    }
+
 }
