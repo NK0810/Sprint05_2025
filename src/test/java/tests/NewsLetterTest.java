72eb72e08e95ce6444cae41b79e0a1f9d8a9329c
[PROJECT <[PROJECT>
2025-05-18 13:56:47 +0300

    456

--- src/test/java/tests/NewsLetterTest.java
+++ src/test/java/tests/NewsLetterTest.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
-public class NewsLetterTestgit  extends BaseTest {
+public class NewsLetterTest extends BaseTest {
     private static final String TEST_EMAIL = "test@email.com";
 
     @Description("Subscribe on newsletter")
