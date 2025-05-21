f107addbbd2c4a203031bdb53de9ba0c33c35e78
igors <igor.stolenko@gmail.com>
2025-05-21 13:22:33 +0300

    page and test classes created

--- src/main/java/pages/UserAddressesPage.java
+++ src/main/java/pages/UserAddressesPage.java
@@ -8,11 +8,15 @@ public class UserAddressesPage extends BasePage<UserAddressesPage>{
     private final CustomerSidebarFragment customerSidebarFragment;
 
     public enum UserAddressesPageElements {
-
         DEAFAULT_DELIVERY_ADDRESS_INFO_BLOCK("(//div/address)[1]"),
         DEAFAULT_PAYMENT_ADDRESS_INFO_BLOCK("(//div/address)[2]"),
         OTHER_DELIVERY_ADDRESS_INFO_BLOCK("(//div/address)[3]"),
-        OTHER_PAYMENT_ADDRESS_INFO_BLOCK("(//div/address)[4]");
+        OTHER_PAYMENT_ADDRESS_INFO_BLOCK("(//div/address)[4]"),
+        EDIT_DEAFAULT_DELIVERY_ADDRESS_BUTTON("(//a[@class='action edit dashboard-info-block__link'])[1]"),
+        EDIT_DEAFAULT_PAYMENT_ADDRESS_BUTTON("(//a[@class='action edit dashboard-info-block__link'])[2]"),
+        EDIT_OTHER_DELIVERY_ADDRESS_BUTTON("(//a[@class='action edit dashboard-info-block__link'])[3]"),
+        EDIT_OTHER_PAYMENT_ADDRESS_ADDRESS_BUTTON("(//a[@class='action edit dashboard-info-block__link'])[4]");
+
 
         private final By element;
 
