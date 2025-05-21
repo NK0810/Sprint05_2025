b1536e4f2bc9f7f8a73c0a69f9f7cb4122f6a840
igors <igor.stolenko@gmail.com>
2025-05-21 12:44:01 +0300

    page and test classes created

---
+++ src/main/java/pages/UserAddressesPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import fragments.CustomerSidebarFragment;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UserAddressesPage extends BasePage<UserAddressesPage>{
+    private final CustomerSidebarFragment customerSidebarFragment;
+
+    public enum UserAddressesPageElements {
+
+        DEAFAULT_DELIVERY_ADDRESS_INFO_BLOCK("(//div/address)[1]"),
+        DEAFAULT_PAYMENT_ADDRESS_INFO_BLOCK("(//div/address)[2]"),
+        OTHER_DELIVERY_ADDRESS_INFO_BLOCK("(//div/address)[3]"),
+        OTHER_PAYMENT_ADDRESS_INFO_BLOCK("(//div/address)[4]");
+
+        private final By element;
+
+        UserAddressesPageElements(String xpath) {
+            this.element = By.xpath(xpath);
+        }
+
+        public By getLocator() {
+            return element;
+        }
+    }
+
+    public UserAddressesPage(WebDriver driver) {
+        super(driver);
+        this.customerSidebarFragment = new CustomerSidebarFragment(driver);
+    }
+
+    public CustomerSidebarFragment getCustomerSidebarFragment() {
+        return customerSidebarFragment;
+    }
+}
