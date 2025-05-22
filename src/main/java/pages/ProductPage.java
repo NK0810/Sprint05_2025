21b252793a48e7c32fd8b5e0728a3a9ed582d473
igors <igor.stolenko@gmail.com>
2025-05-22 14:47:43 +0300

    refactor changes

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -17,8 +17,8 @@ public class ProductPage extends BasePage<ProductPage> {
         PRODUCT_BRAND("//*[text()='Бренд']/following-sibling::*"),
         BACK_ON_HOME_PAGE("//*[contains(@class,'home')]/*"),
         SELECT_SIZE_DROP_DOWN_BUTTON("//div[@class='visual-ko-select visual-ko-select--rozmiar ']"),
-        REGULAR_PRICE_FIRST_PRODUCT("//span[@class='c-price__omnibus']/span"),
-        CURRENT_PRICE_FIRST_PRODUCT("(//span[@class='price'])[2]"),
+        REGULAR_PRICE_FIRST_PRODUCT("//div[@class='c-price c-price--type-productpage']/span[@class='c-price__omnibus']/span"),
+        CURRENT_PRICE_FIRST_PRODUCT("//div[@class='c-price c-price--type-productpage']//span[@data-price-type='finalPrice']"),
         ADD_TO_WISHLIST_BUTTON("//a[@class='action action-to-wishlist add']"),
         SUCCESS_MASSAGE("//div[@data-ui-id='message-success']");
 
