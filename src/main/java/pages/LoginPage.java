22d61dbf4f9aeb1d0cc51212ce115594f92cf86b
[PROJECT <[PROJECT>
2025-05-22 15:29:26 +0300

    e3214132423

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 import org.openqa.selenium.WebElement;
