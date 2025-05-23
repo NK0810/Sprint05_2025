342017fe806719944f8ec5010603854735716d2c
[PROJECT <[PROJECT>
2025-05-23 12:14:40 +0300

    Resolved3232

--- src/test/java/tests/EditPersonalData.java
+++ src/test/java/tests/EditPersonalData.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
@@ -9,31 +10,30 @@ import pages.LoginPage;
 import pages.UserAccountPage;
 import utils.ConfigReader;
 
-    public class EditPersonalData extends BaseTest {
+public class EditPersonalData extends BaseTest {
     private static final String BASE_NAME = "Anton";
     private static final String BASE_SURNAME = "Skvortsov";
     private static final String ERROR_MESSAGE = "The save message did not appear or is incorrect.";
     private static final String EXPECTED_RESULT = "Дані Вашого Облікового запису збережено.";
     private static final String NEW_NAME = BASE_NAME + generateRandomLetter();
+    private static final String NEW_SURNAME = BASE_SURNAME + generateRandomLetter();
 
     @Description("Edit user name and choose agreements")
     @Test
     public void EditPersonalDataTest() throws InterruptedException {
 
         HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        UserAccountPage userAccountPage = new UserAccountPage(driver);
 
         homePage.openUrl()
                 .acceptCookies();
-
-        LoginPage loginPage = new LoginPage(driver);
 
         loginPage.openLoginPage()
                 .enterEmail(ConfigReader.getProperty("test.email"))
                 .enterPassword(ConfigReader.getProperty("test.password"))
                 .clickLogInButton();
 
-
-        UserAccountPage userAccountPage = new UserAccountPage(driver);
         userAccountPage.clickMyAccountSetting()
                 .enterNewUserName(NEW_NAME)
                 .enterNewUserSurname(NEW_SURNAME)
@@ -51,15 +51,13 @@ import utils.ConfigReader;
         String actualSurname = userAccountPage.getSurnameFieldText().trim();
         Assert.assertEquals(actualName, NEW_NAME, "The name has been updated correctly.");
         Assert.assertEquals(actualSurname, NEW_SURNAME, "The name was updated incorrectly.");
-
         Assert.assertTrue(userAccountPage.isCheckBoxMailSelected(), "The Email checkbox was not saved.");
         Assert.assertTrue(userAccountPage.isCheckBoxPhoneSelected(), "The Phone checkbox was not saved.");
     }
-    private static final String NEW_SURNAME = BASE_SURNAME + generateRandomLetter();
 
     private static String generateRandomLetter() {
         return String.valueOf((char) ('A' + (int) (Math.random() * 26)));
-     }
     }
+}
 
 
